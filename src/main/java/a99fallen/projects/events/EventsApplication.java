ad992e66082875c19e3512e0a5f8837387697214
fallen_a99 <fallen_a99@o2.pl>
2020-12-05 11:21:53 +0100

    #1 basic :: inicjalizacja spring boota

---
+++ src/main/java/a99fallen/projects/events/EventsApplication.java
@@ -0,0 +1,13 @@
+package a99fallen.projects.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsApplication.class, args);
+	}
+
+}
