2a3a8ec08932ebcf1bdb7de3300376f9f57bd3cf
fallen_a99 <fallen_a99@o2.pl>
2020-12-06 12:10:32 +0100

    login :: poprawienie metod

--- src/main/java/a99fallen/projects/events/web/controller/HomePageController.java
+++ src/main/java/a99fallen/projects/events/web/controller/HomePageController.java
@@ -1,6 +1,5 @@
 package a99fallen.projects.events.web.controller;
 
-import a99fallen.projects.events.domain.model.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -8,13 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j @RequiredArgsConstructor
-public class UserAccountController {
+public class HomePageController {
 
-    private User user;
-
-    @GetMapping("/account")
-    public String getUserAccount() {
-
-    return "/account";
+    @GetMapping
+    public String getHomepage() {
+        return "/";
     }
 }
