2ed4d5386d8f330a99d06cc0a31c6c4c59a63cc7
a99fallen <fallen_a99@o2.pl>
2020-12-10 23:25:08 +0100

    tasks :: strona z formularzem i poprawka adresu w kontrolerze

--- src/main/java/a99fallen/projects/events/web/controller/AddNewTaskController.java
+++ src/main/java/a99fallen/projects/events/web/controller/AddNewTaskController.java
@@ -9,14 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/tasks")
+@RequestMapping("/task")
 @Slf4j
 @RequiredArgsConstructor
 public class AddNewTaskController {
 
-    @GetMapping
+    @GetMapping("/add")
     public String getAddTaskPage(Model model) {
         model.addAttribute(new CreateTaskCommand());
-        return "/tasks/add";
+        return "/task/add";
     }
 }
