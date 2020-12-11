1b7a26546229f7afe5658462c2fb89ff9894b7f0
a99fallen <fallen_a99@o2.pl>
2020-12-12 00:05:52 +0100

    tasks :: stworzenie serwisu i konwertera zadań z poprawkami innych klas

--- src/main/java/a99fallen/projects/events/domain/repository/TaskRepository.java
+++ src/main/java/a99fallen/projects/events/domain/repository/TaskRepository.java
@@ -1,7 +1,15 @@
 package a99fallen.projects.events.domain.repository;
 
 import a99fallen.projects.events.domain.model.Task;
+import a99fallen.projects.events.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findAllByUsersUsername(User username);
+
+    Optional<Task> findByName(String sourceName);
 }
---
+++ src/main/java/a99fallen/projects/events/sevice/TaskService.java
@@ -0,0 +1,41 @@
+package a99fallen.projects.events.sevice;
+
+import a99fallen.projects.events.converter.TaskConverter;
+import a99fallen.projects.events.domain.model.Task;
+import a99fallen.projects.events.domain.model.User;
+import a99fallen.projects.events.domain.repository.TaskRepository;
+import a99fallen.projects.events.domain.repository.UserRepository;
+import a99fallen.projects.events.web.command.CreateTaskCommand;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+    private final UserRepository userRepository;
+    private final TaskConverter taskConverter;
+
+    public void add(CreateTaskCommand createTaskCommand) {
+        log.debug("Dane do utworzenia zadania: {}", createTaskCommand);
+
+        Task task = taskConverter.from(createTaskCommand);
+        updateTaskWithUser(task);
+        log.debug("Zadanie do zapisu: {}", task);
+
+        taskRepository.save(task);
+        log.debug("Zapisane zadanie: {}", task);
+    }
+
+    private void updateTaskWithUser(Task task) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.getAuthenticatedUser(username);
+        task.setUsers((Set<User>) user);
+    }
+}
