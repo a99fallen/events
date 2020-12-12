60b7b7d8375fb920149553a5f88b30bdcd102813
a99fallen <fallen_a99@o2.pl>
2020-12-12 09:53:14 +0100

    tasks :: ostatnia działająca konfiguracja

--- src/main/java/a99fallen/projects/events/domain/model/User.java
+++ src/main/java/a99fallen/projects/events/domain/model/User.java
@@ -3,7 +3,9 @@ package a99fallen.projects.events.domain.model;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
@@ -20,7 +22,22 @@ public class User {
     private String password;
 //    private Boolean active = Boolean.FALSE;
 
-    @ManyToMany (mappedBy = "users")
-    private List<Task> tasks;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(
+                    name = "username",
+                    referencedColumnName = "username"),
+            indexes = @Index(
+                    name = "user_roles_username_idx",
+                    columnList = "username"))
+    @Column(name = "role")
+    private Set<String> roles;
 
+    @ManyToMany
+    @JoinTable(
+            name = "user_tasks",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "task_id"))
+    private List<Task> tasks = new ArrayList<>();
 }
--- src/main/java/a99fallen/projects/events/web/controller/UserAccountController.java
+++ src/main/java/a99fallen/projects/events/web/controller/UserAccountController.java
@@ -5,16 +5,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j @RequiredArgsConstructor
+@RequestMapping("/account")
 public class UserAccountController {
 
     private User user;
 
-    @GetMapping("/account")
+    @GetMapping
     public String getUserAccount() {
 
-    return "/account";
+    return "user/account";
     }
 }
