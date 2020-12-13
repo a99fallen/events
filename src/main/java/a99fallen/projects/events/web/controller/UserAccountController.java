fd7f4ded7fbb9176b995470d58725771b8d1f0b6
MZdebel <malgorzata.zdebel@gmail.com>
2020-12-13 15:09:41 +0100

    user-account :: add-task method

--- src/main/java/a99fallen/projects/events/web/controller/UserAccountController.java
+++ src/main/java/a99fallen/projects/events/web/controller/UserAccountController.java
@@ -1,21 +1,29 @@
 package a99fallen.projects.events.web.controller;
 
 import a99fallen.projects.events.domain.model.User;
+import a99fallen.projects.events.web.command.CreateTaskCommand;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j @RequiredArgsConstructor
-@RequestMapping("/account")
+@RequestMapping
 public class UserAccountController {
 
     private User user;
 
-    @GetMapping
+    @GetMapping("/account")
     public String getUserAccount() {
     return "user/account";
+    }
+
+    @GetMapping("/add")
+    public String getAddTaskPage(Model model) {
+        model.addAttribute(new CreateTaskCommand());
+        return "/task/add";
     }
 }
